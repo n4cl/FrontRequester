4f17a8bf686d768ef9d65b8bdbb937296f515db8
n4cl <devn4cl@gmail.com>
2020-08-12 01:01:43 +0900

    Update: ログ出力を追加

--- back_server/app/server.py
+++ back_server/app/server.py
@@ -1,7 +1,19 @@
 from flask import Flask, jsonify
 
+from logging import getLogger, StreamHandler, Formatter, DEBUG
+
+logger = getLogger(__name__)
+formatter = Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(name)s - %(funcName)s - %(message)s')
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+handler.setFormatter(formatter)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    logger.debug('hello')
     return jsonify({"response": "hello world"}), 200
